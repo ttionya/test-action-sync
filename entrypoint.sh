11322cffdb97802cefa72f8bbc3b44b446179959
ttionya <git@ttionya.com>
2020-06-02 00:10:47 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,16 @@ echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
 echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
 echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
 echo "GITHUB_WORKFLOW: ${GITHUB_WORKFLOW}"
+
+ls
+
+git remote add target ${target_repository}
+git remote -v
+
+if [[ "${GITHUB_EVENT_NAME}" == "push" ]]; then
+  git push target "${GITHUB_REF}:${GITHUB_REF}" -f --tags
+fi
+
+if [[ "${GITHUB_EVENT_NAME}" == "delete" ]]; then
+  echo "delete"
+fi
